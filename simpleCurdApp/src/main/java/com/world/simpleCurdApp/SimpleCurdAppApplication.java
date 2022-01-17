c434faf03dd498fac9e62c1f7ad31220994266dc
Ehab Melhem <Melhemehab@gmail.com>
2022-01-17 10:52:22 +0200

    commit
    commit

---
+++ simpleCurdApp/src/main/java/com/world/simpleCurdApp/SimpleCurdAppApplication.java
@@ -0,0 +1,13 @@
+package com.world.simpleCurdApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCurdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCurdAppApplication.class, args);
+	}
+
+}
