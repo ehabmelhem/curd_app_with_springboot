0c5f3788d8bb2a8367b54c0fe46a613df37707df
Ehab Melhem <Melhemehab@gmail.com>
2022-01-17 10:52:22 +0200

    commit
    commit

---
+++ simpleCurdApp/src/test/java/com/world/simpleCurdApp/SimpleCurdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.simpleCurdApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCurdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
